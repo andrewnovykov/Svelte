db80acb8b20171b3ef930c1dad660e0b809f7653
papapin <papapin777#gmail.com>
2019-06-03 21:00:48 -0700

    getting started

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,20 @@
 <script>
-	export let name;
+	let name = 'Andrew';
+	let age = 32;
+
+	function incAge () {
+		age += 1;
+	}
 </script>
 
 <style>
+
 	h1 {
 		color: purple;
 	}
+
 </style>
 
 <h1>Hello {name}!</h1>
+<h1>My age is {age}</h1>
+<button on:click={incAge}> Change Age  </button>
